ae7b85ecc3bef11a6a6c3c72075d1f23c9e42c92
Duncan Kariuki <duncankariukimugo@gmail.com>
2025-05-21 10:38:00 +0300

    finished labs

--- lib/debug.py
+++ lib/debug.py
@@ -4,6 +4,29 @@
 from __init__ import CONN, CURSOR
 from department import Department
 
+
 import ipdb
+
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A")
+print(payroll)
+
+payroll.save()
+print(payroll)
+
+hr = Department.create("HR", "Building B")
+print(hr)
+
+hr.save()
+print(hr)
+
+hr.name="Human Resources"
+hr.location="Building F"
+hr.update()
+print(hr)
+payroll.delete()
+print()
 
 ipdb.set_trace()
